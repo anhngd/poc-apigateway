3f07ea1dc37d91c41e9ed72ef6aa56fd1b2623f2
Nguyen Anh <anhnd@bravezone.com>
2022-03-26 17:10:06 +0700

    Add gitignore

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Serilog;
+
+namespace ApiGateway;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .CreateLogger();
+            
+        Log.Information("Hello, Serilog!");
+        Log.CloseAndFlush();
+        
+        new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s =>
+            {
+                s.AddOcelot()
+                .AddCacheManager(x =>
+                {
+                    x.WithDictionaryHandle();
+                });
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                //  add your logging
+                logging.AddConsole();
+            })
+            // .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseMiddleware<RequestResponseLoggingMiddleware>();
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
+    }
+}
---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Administration;
+using Ocelot.DependencyInjection;
+
+namespace ApiGateway;
+
+public class Startup
+{
+    public virtual void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddOcelot()
+            .AddAdministration("/administration", "secret");
+    }
+}
